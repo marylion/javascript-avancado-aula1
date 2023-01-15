f0e60f5f99a780eae454c454af78c2c8556bece6
marylion <mariabiatriz.mb13@gmail.com>
2023-01-15 16:07:52 -0300

    criação de array, objeto e e função

---
+++ arrays.js
@@ -0,0 +1,24 @@
+alert("Oi Maria, vamos treinar arrays hoje?");
+
+const nomes = ['Maria', 'Iraneide', 'Irani', 'Ivanilde', 'Irene'];
+console.log(nomes);
+
+//Podemos escrever os conjuntos de dados um embaixo do ondurationchange, com a quebra de linha.
+//const nomes = [
+//     'Maria', 
+//     'Iraneide', 
+//     'Irani', 
+//     'Ivanilde', 
+//     'Irene'
+// ];
+
+//------Manipulando a Array com o método Join(), que é usada para junção de elementos de uma dado array em uma string, retornando esta mesma string, a partir do uso de um separador que pode ser espaço(' '), vírgula (',') que é o padrão, um asterístico ('*'), ou outro separador especificado.
+
+console.log(nomes.join('*'));
+console.log(nomes.join('--X--'));
+console.log(nomes.join(' '));
+console.log(nomes.join(' & '));
+
+
+
+
---
+++ funcao.js
@@ -0,0 +1,7 @@
+// >>>Uma função é entendida como um subconjunto de linhas de códigos, como se fosse um subprograma, que pode ser chamado por outro
+
+// >>>Para criar uma função precisamos utilizar primeiramente “function”, em seguida o nome da função, e entre parênteses os argumentos dessa função
+
+// >>>As funções podem receber argumentos, ou seja, receber dados para usar dentro dos blocos de código
+
+// >>>Uma função pode ter várias linhas e não necessariamente precisa retornar algo ou receber algum argumento.
